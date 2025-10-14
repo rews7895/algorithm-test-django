79b566b961ec14964e27819a92dcc1b82296e4e7
박준석 <nile7895@hani.com>
2025-10-14 10:42:25 +0900

    command 파일 생성

---
+++ codeup/management/commands/codeup_6077.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from utils.support import measure_time_and_memory
+
+
+class Command(BaseCommand):
+    help = "CodeUp 6077"
+
+    @measure_time_and_memory
+    def handle(self, *args, **options):
+        pass
