b8247f8ee806eca5c062b18361a75e10f87223e8
Junseok <yalvz@naver.com>
2025-10-20 21:59:27 +0900

    6073번까지

--- codeup/management/commands/codeup_6073.py
+++ codeup/management/commands/codeup_6073.py
@@ -8,4 +8,23 @@ class Command(BaseCommand):
 
     @measure_time_and_memory
     def handle(self, *args, **options):
-        pass
+        """
+        본 문제는 python 의 빠른 기초 학습을 위해 설계된 문제로서 python 코드 제출을 기준으로 설명되어 있습니다.
+        ------
+
+        정수(1 ~ 100) 1개가 입력되었을 때 카운트다운을 출력해보자.
+
+        while 조건식 :
+        ...
+        ...
+
+        반복 실행구조를 사용해 보자.
+
+        참고
+        조건검사, 출력, 감소의 순서와 타이밍을 잘 생각해보자.
+        """
+        s = int(input())
+        while s > 0:
+            s = s - 1
+            print(s)
+
